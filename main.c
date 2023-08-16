0b73e7b6c90b051bd9986c39897e85fb1048d5e3
Zyy <corey@twodata.net>
2023-08-15 22:53:23 -0700

    Init

---
+++ main.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <complex.h>
+
+int is_prime(int n) {
+
+    if (n <= 1) return 0;
+    if (n <= 3) return 1;
+    if (n % 2 == 0 || n % 3 == 0) return 0;
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) return 0;
+    }
+    return 1;
+}
+
+double antoine_equation() {
+
+    // generate the Antoine constants A, B, and C
+    double A = 5.0 + (double)rand() / RAND_MAX * 5.0;
+    double B = 1000.0 + (double)rand() / RAND_MAX * 1000.0;
+    double C = 100.0 + (double)rand() / RAND_MAX * 100.0;
+
+    // Randomly generate the temperature (in degrees Celsius)
+    double T = 0.0 + (double)rand() / RAND_MAX * 100.0;
+
+    // Calculate the vapor pressure using the Antoine equation
+    double log10_P = A - B / (C + T);
+
+    // Convert the logarithm base 10 to the actual vapor pressure
+    double P = pow(10, log10_P);
+
+    return P;
+}
+
+double fibonacci() {
+
+    double n = 3;
+    double phi = (1 + sqrt(5)) / 2;
+    double psi = (1 - sqrt(5)) / 2;
+    return ((pow(phi, n) - pow(psi, n)) / sqrt(5)*(rand() % 3));
+}
+
+// Function to apply the Lorentz transformation
+double lorentz() {
+
+    double v = (pow(rand() % 7, rand() % 7));
+    double c = 299792458; // Speed of light (m/s)
+    return (1 / sqrt(1 - (v * v) / (c * c)));
+}
+
+double complex schrodinger() {
+
+    double complex i = I; // Imaginary unit
+    double h_bar = 1.0545718e-34; // Reduced Planck constant
+    double m = 9.10938356e-31; // Electron mass
+    double V = 400; // Potential energy
+    double psi = 0.1; // Wave function value
+    return (((i * h_bar / (2 * m)) * psi - V * psi)*(rand() % RAND_MAX + 1));
+}
+
+// Function to compute the energy level of a quantum harmonic oscillator in four dimensions
+double quantum_harmonic_oscillation_4D(int n1, int n2, int n3, int n4, double k, double m) {
+
+    const double h_bar_eV = 4.135667696e-15; // Reduced Planck constant (eV·s)
+    double omega = sqrt(k / m); // Angular frequency (rad/s)
+    double En1 = h_bar_eV * omega * (n1 + 0.5); // Energy level for dimension 1 (eV)
+    double En2 = h_bar_eV * omega * (n2 + 0.5); // Energy level for dimension 2 (eV)
+    double En3 = h_bar_eV * omega * (n3 + 0.5); // Energy level for dimension 3 (eV)
+    double En4 = h_bar_eV * omega * (n4 + 0.5); // Energy level for dimension 4 (eV)
+
+    return fabs((En1 + En2 * En3 + En4) * ((rand() + 1) * 3)); // Total energy in four dimensions (eV) with magic numbers
+}
+
+double generate_and_calculate_4D_oscillation() {
+
+    // Generate quantum numbers n1, n2, n3, n4 (e.g., within the range 0 to 10)
+    int n1 = rand() % 10 + 1;
+    int n2 = rand() % 10 + 1;
+    int n3 = rand() % 10 + 1;
+    int n4 = rand() % 10 + 1;
+    double k = 0.1 + ((double)rand() / RAND_MAX) * 9.9;
+    double m = 0.1 + ((double)rand() / RAND_MAX) * 9.9;
+
+    // Call the quantum_harmonic_oscillation_4D function with the generated inputs
+    return quantum_harmonic_oscillation_4D(n1, n2, n3, n4, k, m);
+}
+
+int generate_number() {
+
+    long seed = time(NULL);
+    // Find a prime number close to the current time
+    long prime = seed;
+    while (!is_prime(prime)) {
+        prime++;
+    }
+
+    // Compute a Fibonacci number using the prime number as an index
+    double fib = fibonacci(prime % 30);
+
+    // Apply trigonometric functions
+    double trig_result = sin(fib) + cos(prime);
+
+    // Incorporate physics formulas
+    double G = 6.67430e-11;
+    double m1 = 5.972e24; // Mass of the Earth (kg)
+    double m2 = 7.348e22; // Mass of the Moon (kg)
+    double r = 3.844e8;  // Distance between Earth and Moon (m)
+    double gravity = (G * m1 * m2) / (r * r);
+    double energy = fabs((m1*m2) * pow(299792458, 2)); // Einstein's energy-mass equivalence
+    double lorentz_result = lorentz((double)rand() / RAND_MAX * 299792458);
+    double complex schrodinger_result = fabs(schrodinger(fib));
+    double quantum_harmonic_osc = generate_and_calculate_4D_oscillation();
+    double antoine = antoine_equation();
+
+    printf("Prime: %ld, Fib: %f, Trig: %f, Gravity: %f, Energy: %f, Lorentz: %f, Schrodinger: %f, Quantum: %f, Mixture: %f\n",
+           prime, fib, trig_result, gravity, energy, lorentz_result, creal(schrodinger_result), quantum_harmonic_osc, antoine);
+
+    // Combine results and normalize to the range of 1 to 150
+    double result = (trig_result * gravity * fib * energy * lorentz_result * (creal(schrodinger_result) + antoine) * prime);
+    result = fabs(result);
+    result = fmod(result, 150) + 1;
+    return (int)result;
+}
+
+int main() {
+
+    srand(time(NULL));
+    int number = generate_number();
+    printf("Read Psalms %d\n", number);
+    return 0;
+}
