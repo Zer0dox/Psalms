59a7819d85e61909c0f79b34bfc5bb9bef3926f8
Zyy <corey@twodata.net>
2023-08-15 23:31:20 -0700

    Init

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/zyantos/CLionProjects/Psalms
+# Install script for directory: /Volumes/G-DRIVE ArmorATD/Software Engineering/Psalms
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/zyantos/CLionProjects/Psalms/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Volumes/G-DRIVE ArmorATD/Software Engineering/Psalms/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
